88ef1110e5e60577323a0f155400670754873d13
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-08-28 15:09:06 +0430

    Create hover.tsx

---
+++ components/core/hover.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const hover = () => {
+  return (
+    <button className="bg-blue-500 text-white hover:bg-blue-600 text-bold p-3 rounded-md">
+      Hover Me
+    </button>
+  );
+};
+
+export default hover;
