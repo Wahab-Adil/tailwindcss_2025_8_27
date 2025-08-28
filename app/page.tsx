08107a2e7cecea3f23a0ff218b92eb357b803b55
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-08-28 15:09:24 +0430

    layout and main page style

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import CenterContainer from "@/components/utils/CenterContainer";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="w-full flex flex-col  border">
+      <CenterContainer
+        children={
+          <div className="text-center my-3">
+            <h1 className="text-4xl text-blue-500 text-bold ">Core Concepts</h1>
+            <Link
+              href="core"
+              className="inline-block px-6 py-2 bg-blue-200 text-black text-bold font-semibold rounded hover:bg-blue-300 transition duration-300 w-[150px] h-[40px] text-center"
+            >
+              Core
+            </Link>
+          </div>
+        }
+      />
+    </div>
+  );
+}
