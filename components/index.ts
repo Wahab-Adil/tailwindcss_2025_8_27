f1dc55f25220a65d015ad6f156f5f2b74d6cb853
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-08-29 20:03:05 +0430

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
 import Hover from "@/components/core/hover";
+import Focus from "@/components/core/focus";
+import FirstChild from "@/components/core/FirsChild";
+import Required from "@/components/core/required";
 
-export { Hover };
+export { Hover, Focus, FirstChild, Required };
