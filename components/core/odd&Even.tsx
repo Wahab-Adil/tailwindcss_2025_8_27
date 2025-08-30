acb67f1e23eb12d0866115cf424dd46cb8c7fbb9
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-08-30 20:48:53 +0430

    Create odd&Even.tsx

---
+++ components/core/odd&Even.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const AddAndEven = () => {
+  return (
+    <div>
+      <table className="border">
+        <tbody>
+          <tr className="odd:text-white odd:bg-amber-400 even:text-white even:bg-sky-400">
+            <td>Jane Cooper</td>
+            <td>Regional Paradigm Technician</td>
+            <td>jane.cooper@example.com</td>
+          </tr>
+          <tr className="odd:text-white odd:bg-amber-400 even:text-white even:bg-sky-400">
+            <td>Jane Cooper</td>
+            <td>Regional Paradigm Technician</td>
+            <td>jane.cooper@example.com</td>
+          </tr>
+          <tr className="odd:text-white odd:bg-amber-400 even:text-white even:bg-sky-400">
+            <td>Jane Cooper</td>
+            <td>Regional Paradigm Technician</td>
+            <td>jane.cooper@example.com</td>
+          </tr>
+          <tr className="odd:text-white odd:bg-amber-400 even:text-white even:bg-sky-400">
+            <td>Jane Cooper</td>
+            <td>Regional Paradigm Technician</td>
+            <td>jane.cooper@example.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AddAndEven;
