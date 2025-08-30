179ecf54f2e3d9e172934ed71a9a164eda0b9481
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-08-30 20:49:16 +0430

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,20 @@ import Hover from "@/components/core/hover";
 import Focus from "@/components/core/focus";
 import FirstChild from "@/components/core/FirsChild";
 import Required from "@/components/core/required";
+import BeforeAndAfter from "@/components/core/before&after";
+import Placeholder from "@/components/core/placeholder";
+import Selection from "@/components/core/selection";
+import OddEven from "@/components/core/odd&Even";
+import Has from "@/components/core/has";
 
-export { Hover, Focus, FirstChild, Required };
+export {
+  Hover,
+  Focus,
+  FirstChild,
+  Required,
+  BeforeAndAfter,
+  Placeholder,
+  Selection,
+  OddEven,
+  Has,
+};
