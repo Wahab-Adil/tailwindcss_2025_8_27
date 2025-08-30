7add5f42ee661dd047634b240bb7fbd9db81800f
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-08-30 20:49:08 +0430

    Update page.tsx

--- app/core/page.tsx
+++ app/core/page.tsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { Hover, Focus, FirstChild, Required } from "@/components";
+import {
+  Hover,
+  Focus,
+  FirstChild,
+  Required,
+  BeforeAndAfter,
+  Placeholder,
+  Selection,
+  OddEven,
+  Has,
+} from "@/components";
 import CenterContainer from "@/components/utils/CenterContainer";
 
 const page = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold text-center">Core Concepts</h1>
+      <CenterContainer title="Has Functionality" children={<Has />} />
+      <CenterContainer
+        title="Odd And Even Functionality"
+        children={<OddEven />}
+      />
+      <CenterContainer
+        title="Before And After Functionality"
+        children={<BeforeAndAfter />}
+      />
+      <CenterContainer
+        title="Placeholder Functionality"
+        children={<Placeholder />}
+      />
+      <CenterContainer
+        title="Selection Functionality"
+        children={<Selection />}
+      />
       <CenterContainer title="Hover Functionality" children={<Hover />} />
       <CenterContainer title="Focus Functionality" children={<Focus />} />
       <CenterContainer
